1d533365be8ec377575b9f3de48ebd719d80caef
Richard Kamara <kamara.richard10@gmail.com>
2024-12-03 21:55:55 +0200

    Add more rooms into data loader.

--- src/main/java/com/company/hotel/service/DataLoader.java
+++ src/main/java/com/company/hotel/service/DataLoader.java
@@ -25,19 +25,26 @@ public class DataLoader implements CommandLineRunner {
     private void createRooms() {
         if (roomRepository.count() == 0) {
             // Load premium rooms
-            Room room1 = new Room(RoomCategory.PREMIUM, Boolean.FALSE); // Room is not occupied
+            Room room1 = new Room(RoomCategory.PREMIUM, Boolean.FALSE); // Room is free
             Room room2 = new Room(RoomCategory.PREMIUM, Boolean.FALSE);
-            Room room3 = new Room(RoomCategory.PREMIUM, Boolean.TRUE);  // Room is free
+            Room room3 = new Room(RoomCategory.PREMIUM, Boolean.TRUE);  // Room is not available
             Room room4 = new Room(RoomCategory.PREMIUM, Boolean.FALSE);
+            Room room5 = new Room(RoomCategory.PREMIUM, Boolean.FALSE);
+            Room room6 = new Room(RoomCategory.PREMIUM, Boolean.FALSE);
+            Room room7 = new Room(RoomCategory.PREMIUM, Boolean.FALSE);
+            Room room8 = new Room(RoomCategory.PREMIUM, Boolean.FALSE);
 
             // Load economy rooms
-            Room room5 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
-            Room room6 = new Room(RoomCategory.ECONOMY, Boolean.TRUE);
-            Room room7 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
-            Room room8 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
-            Room room9 = new Room(RoomCategory.ECONOMY, Boolean.TRUE);
+            Room room9 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
+            Room room10 = new Room(RoomCategory.ECONOMY, Boolean.TRUE);
+            Room room11 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
+            Room room12 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
+            Room room13 = new Room(RoomCategory.ECONOMY, Boolean.TRUE);
+            Room room14 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
+            Room room15 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
+            Room room16 = new Room(RoomCategory.ECONOMY, Boolean.FALSE);
 
-            roomRepository.saveAll(List.of(room1, room2, room3, room4, room5, room6, room7, room8, room9));
+            roomRepository.saveAll(List.of(room1, room2, room3, room4, room5, room6, room7, room8, room9, room10, room11, room12, room13, room14, room15, room16));
 
             System.out.println("Data loaded: " + roomRepository.count() + " rooms created.");
         }
